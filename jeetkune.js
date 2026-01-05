0e012fb57ce5f31e834e314b281479f5199ca052
stevemanleyjhorsan <76927265+stevemanleyjhorsan@users.noreply.github.com>
2026-01-05 16:41:11 +0300

    Update jeetkune.js

--- jeetkune.js
+++ jeetkune.js
@@ -11,5 +11,3 @@ document.addEventListener("DOMContentLoaded", function() {
 	var rtlMode = '0';
 	var logged = 0;
 	var utk = '0';
-var system = {"error":"An error has occured","access_requirement":"You do not meet the requirements to access this room","empty_field":"Please fill out all fields","no_file":"You must select a file","file_big":"Your file is too large","action_complete":"Action completed","select_something":"Please select something","cannot_contact":"You currently cannot contact this user","site_connect":"Please connect to the site to enter chat","missing_recaptcha":"Please complete the reCAPTCHA"};
-var notification = {"user_join":"Is connected"};
